11dbc1ae781bbb83bd7b2d614e4822d5b9645cdb
elizatu <eliza.turco@gmail.com>
2018-07-26 19:22:16 +0000

    automatically backed up by learn

--- nesting.rb
+++ nesting.rb
@@ -13,4 +13,4 @@
 closet = { "tops"=> ["rain jacket", "cardigan", "blazer"]
 "bottoms" => ["jeans", "skirts", "capris"]
 "footwear" => ["sneakers", "heels", "flats"]}
-puts closet[1]
+puts closet["tops"]
