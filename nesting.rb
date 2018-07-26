5d9c79a009d981f8a01b64abe0d8ecae63e9f6b5
elizatu <eliza.turco@gmail.com>
2018-07-26 19:21:51 +0000

    automatically backed up by learn

--- nesting.rb
+++ nesting.rb
@@ -1,10 +1,16 @@
-tops = ["blouses","t-shirts","jackets"]
-bottoms = ["jeans","skirts","shorts"]
-footwear = ["sandals","sneakers","heels"]
+# tops = ["blouses","t-shirts","jackets"]
+# bottoms = ["jeans","skirts","shorts"]
+# footwear = ["sandals","sneakers","heels"]
 
-clothes = [tops,bottoms,footwear]
-footwear << "bikenstocks"
-bottoms << "skorts"
-tops << "tube"
-puts clothes[2][1]
+# clothes = [tops,bottoms,footwear]
+# footwear << "bikenstocks"
+# bottoms << "skorts"
+# tops << "tube"
+# puts clothes[2][1]
 #arrays have no key values
+
+
+closet = { "tops"=> ["rain jacket", "cardigan", "blazer"]
+"bottoms" => ["jeans", "skirts", "capris"]
+"footwear" => ["sneakers", "heels", "flats"]}
+puts closet[1]
